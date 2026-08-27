6fbd716e1defafed12de4f0a23c6c043eb13412f
wassim nassour <wassim.nassour@yakeey.com>
2026-08-27 20:27:09 +0100

    init

---
+++ src/test/java/com/example/eventBus/EventBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
